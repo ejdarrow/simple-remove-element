88cedf3049a65a1dd3d4909728f133c51a5ff29d
Arya <arya@foursquare.com>
2019-10-30 16:48:04 -0400

    1.0.2 reduce contexts

--- background.js
+++ background.js
@@ -3,14 +3,14 @@
 chrome.runtime.onInstalled.addListener(() => {
   chrome.contextMenus.create({
     type: 'normal',
-    contexts: ['all'],
+    contexts: ['page', 'frame', 'selection', 'link', 'editable', 'image', 'video', 'audio'],
     id: 'ace7e793-2419-4ab2-b6d6-ece8bf0ce305',
     title: 'Remove Element',
   });
 });
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
-  if (info.menuItemId === "ace7e793-2419-4ab2-b6d6-ece8bf0ce305") {
+  if (info.menuItemId === 'ace7e793-2419-4ab2-b6d6-ece8bf0ce305') {
     chrome.tabs.sendMessage(tab.id, 'remove_element_ace7e793-2419-4ab2-b6d6-ece8bf0ce305');
   }
 });
