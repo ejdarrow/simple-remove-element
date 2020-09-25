5d287dc00fbf1b877cf727c7848e60a438aaad1c
Emily Jade Darrow <ejdarrow@icloud.com>
2020-09-25 12:54:14 -0400

    namespaces are fun

--- undisable_element.js
+++ undisable_element.js
@@ -1,22 +1,22 @@
 'use strict';
 
-let chosenElement = null;
+let selectedElement = null;
 let maxDepth = 4;
 
 document.addEventListener('contextmenu', (event) => {
-  chosenElement = event.target;
+  selectedElement = event.target;
 });
 
 chrome.extension.onMessage.addListener((request) => {
   let depth = 0;
   if (request === 'undisable_element_ace7e793-2419-4ab2-b6d6-ece8bf0ce306') {
     while(depth <= maxDepth) {
-      if (chosenElement.hasAttribute('disabled')){
-        chosenElement.removeAttribute('disabled');
+      if (selectedElement.hasAttribute('disabled')){
+        selectedElement.removeAttribute('disabled');
         break;	
       } else {
         depth++;
-        chosenElement = chosenElement.parentElement;
+        selectedElement = selectedElement.parentElement;
       }
     }
   }
