c0ea3a1219e72ac94e7b6d9c8ad1b05488c5066b
Emily Jade Darrow <ejdarrow@icloud.com>
2020-09-25 12:46:46 -0400

    Adding undisable functionality

--- background.js
+++ background.js
@@ -7,10 +7,18 @@ chrome.runtime.onInstalled.addListener(() => {
     id: 'ace7e793-2419-4ab2-b6d6-ece8bf0ce305',
     title: 'Remove Element',
   });
+  chrome.contextMenus.create({
+    type: 'normal',
+    contexts: ['page', 'frame', 'selection', 'link', 'editable', 'image', 'video', 'audio'],
+    id: 'ace7e793-2419-4ab2-b6d6-ece8bf0ce306',
+    title: 'Un-disable Element',
+  });
 });
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
   if (info.menuItemId === 'ace7e793-2419-4ab2-b6d6-ece8bf0ce305') {
     chrome.tabs.sendMessage(tab.id, 'remove_element_ace7e793-2419-4ab2-b6d6-ece8bf0ce305');
+  } else if (info.menuItemId === 'ace7e793-2419-4ab2-b6d6-ece8bf0ce306') {
+    chrome.tabs.sendMessage(tab.id, 'undisable_element_ace7e793-2419-4ab2-b6d6-ece8bf0ce306');
   }
 });
---
+++ undisable_element.js
@@ -0,0 +1,23 @@
+'use strict';
+
+let chosenElement = null;
+let maxDepth = 4;
+
+document.addEventListener('contextmenu', (event) => {
+  chosenElement = event.target;
+});
+
+chrome.extension.onMessage.addListener((request) => {
+  let depth = 0;
+  if (request === 'undisable_element_ace7e793-2419-4ab2-b6d6-ece8bf0ce306') {
+    while(depth <= maxDepth) {
+      if (chosenElement.hasAttribute('disabled'){
+        chosenElement.removeAttribute('disabled');
+        break;	
+      } else {
+        depth++;
+        chosenElement = chosenElement.parentElement;
+      }
+    }
+  }
+});
