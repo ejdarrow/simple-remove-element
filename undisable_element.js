b5afa90479d9e4528cd558f60df5759499904a6e
Emily Jade Darrow <ejdarrow@icloud.com>
2020-09-25 12:51:38 -0400

    Typo

--- undisable_element.js
+++ undisable_element.js
@@ -11,7 +11,7 @@ chrome.extension.onMessage.addListener((request) => {
   let depth = 0;
   if (request === 'undisable_element_ace7e793-2419-4ab2-b6d6-ece8bf0ce306') {
     while(depth <= maxDepth) {
-      if (chosenElement.hasAttribute('disabled'){
+      if (chosenElement.hasAttribute('disabled')){
         chosenElement.removeAttribute('disabled');
         break;	
       } else {
